6e28034097a5b70a1cf799120222050418928eb9
Minhaz <sharif35-3001@diu.edu.bd>
2022-11-25 17:06:39 +0600

    feat: bus transport structure added

--- src/pages/inbox/Inbox.jsx
+++ src/pages/inbox/Inbox.jsx
@@ -1,5 +1,11 @@
 const Inbox = () => {
-	return <div>Inbox</div>;
+	return (
+		<iframe
+			src="https://www.google.com/maps/d/u/0/embed?mid=1U4zoybqihcU3jzp-w7ULMLEXWJ7Lq0E&ehbc=2E312F"
+			width="640"
+			height="480"
+		></iframe>
+	);
 };
 
 export default Inbox;
--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 // import DashboardRoute from "../pages/dashboard/DashboardRoute";
-import Home from "./../pages/home/Home";
-import Dashboard from "./../pages/dashboard/Dashboard";
-import Inbox from "./../pages/inbox/Inbox";
-import Error from "./../pages/error/Error";
+import Home from "../pages/home/Home";
+import Dashboard from "../pages/dashboard/Dashboard";
+import Inbox from "../pages/inbox/Inbox";
+import Error from "../pages/error/Error";
+import Transport from "../pages/transport/Transport";
 
 const Routers = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/dashboard" element={<Dashboard />} />
+			<Route path="/transport" element={<Transport />} />
 			<Route path="/inbox" element={<Inbox />} />
 			<Route path="*" element={<Error />} />
 			{/* <Route path="/dashboard" element={< DashboardRoute/>} /> */}
